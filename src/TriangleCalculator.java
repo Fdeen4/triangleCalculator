3ddd47c646adf8800737716c671be444ef852db9
GBTC440003UR <GBTC440003UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 17:08:12 -0400

    first commit

---
+++ src/TriangleCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TriangleCalculator {
+    public static void main(String[] args) {
+        double side1, side2, side3;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter side 1: ");
+        side1=scan.nextDouble();
+        System.out.println("Enter side 2: ");
+        side2=scan.nextDouble();
+        System.out.println("Enter side 3: ");
+        side3=scan.nextDouble();
+
+        if (side1==side2 && side1==side3){
+            System.out.println("The triangle is equalateral");
+    }
+    else if((side1*side1)+(side2*side2)==(side3*side3)){
+
+}
+        System.out.println("The triangle is a right triangle");
+    }
+    }
+
+
+
